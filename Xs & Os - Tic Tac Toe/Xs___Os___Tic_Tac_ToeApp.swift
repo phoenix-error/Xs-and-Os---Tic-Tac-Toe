b02c5e70cd10ec3f944f29a9f9d7bb71e1f97447
Luca Becker <42442490+LucaBecker12@users.noreply.github.com>
2023-03-11 19:59:16 +0100

    Initial Commit

---
+++ Xs & Os - Tic Tac Toe/Xs___Os___Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xs___Os___Tic_Tac_ToeApp.swift
+//  Xs & Os - Tic Tac Toe
+//
+//  Created by Luca Becker on 11.03.23.
+//
+
+import SwiftUI
+
+@main
+struct Xs___Os___Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
